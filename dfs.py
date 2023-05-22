5a234745e9699f47fb1819286d7ecff22f0aca2e
AgungPutra220903 <116995331+AgungPutra220903@users.noreply.github.com>
2023-05-22 14:36:48 +0800

    Add files via upload

---
+++ dfs.py
@@ -0,0 +1,18 @@
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['G'],
+  'F' : []
+}
+
+visited = set()
+
+def dfs(visited, graph, node):  #function for dfs 
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+dfs(visited, graph, 'A')
